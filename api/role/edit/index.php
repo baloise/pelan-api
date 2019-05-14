6e7b59f0738a8027fc673e034a56e6b3c474b97d
Elia Reutlinger <elia.reutlinger@gibmit.ch>
2019-05-14 16:35:43 +0200

    Todo: role-edit

---
+++ api/role/edit/index.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once '../../_config/settings.php';
+include_once '../../_config/core.php';
+include_once '../../_config/headers.php';
+
+returnError('This function has not been implemented yet');
